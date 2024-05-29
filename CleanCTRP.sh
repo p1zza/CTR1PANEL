05205c93ae2e4b5c28091ab51ec9cc6a78e6b8f4
p1zza <vovaragulin@gmail.com>
2024-05-29 14:23:20 +0300

    Изменение скрипта для чистки

--- CleanCTRP.sh
+++ CleanCTRP.sh
@@ -2,11 +2,10 @@
 
 echo "Start clean..."
 echo "Clean Application data"
-cd /home/uctf/services/CTR1PANEL
-docker-compose down
-chown -R uctf:uctf /home/uctf/services/CTR1PANEL
-rm -rf /home/uctf/services/CTR1PANEL/db
-rm -rF /home/uctf/services/CTR1PANEL/logs
 
-docker-compose up -d
+docker-compose -f compose.yaml down
+rm -r db
+rm -r logs
+
+docker-compose -f compose.yaml up -d
 echo "Finish clean..."
